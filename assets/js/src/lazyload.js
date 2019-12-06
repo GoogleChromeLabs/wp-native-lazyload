bd3147b215bfbbd2792feb579dbf3edfe4d2487e
Felix Arntz <felixarntz@google.com>
2019-12-06 17:37:50 +0100

    Add support for browsers that do not support dataset (fixes #13).

--- assets/js/src/lazyload.js
+++ assets/js/src/lazyload.js
@@ -18,6 +18,44 @@
  * limitations under the License.
  */
 ( function() {
+	function hyphencase( key ) {
+		return key.replace( /([a-z][A-Z])/g, function( match ) {
+			return match[0] + '-' + match[1].toLowerCase();
+		} );
+	}
+
+	/**
+	 * Gets the value for a data attribute.
+	 *
+	 * @param {DOMNode} elem DOM node.
+	 * @param {string}  key  Data attribute in camel-case.
+	 * @return Data attribute value, or undefined if not set.
+	 */
+	function getData( elem, key ) {
+		if ( elem.dataset ) {
+			return elem.dataset[ key ];
+		}
+		const val = elem.getAttribute( 'data-' + hyphencase( key ) );
+		if ( val === null || val === "" ) {
+			return undefined;
+		}
+		return val;
+	}
+
+	/**
+	 * Deletes a data attribute.
+	 *
+	 * @param {DOMNode} elem DOM node.
+	 * @param {string}  key  Data attribute in camel-case.
+	 */
+	function deleteData( elem, key ) {
+		if ( elem.dataset ) {
+			delete elem.dataset[ key ];
+			return;
+		}
+		elem.removeAttribute( 'data-' + hyphencase( key ) );
+	}
+
 	let lazyElements = [].slice.call( document.querySelectorAll( '.native-lazyload-js-fallback' ) );
 
 	if ( 'IntersectionObserver' in window ) {
@@ -25,19 +63,19 @@
 			entries.forEach( function( entry ) {
 				if ( entry.isIntersecting ) {
 					const lazyElement = entry.target;
-					if ( ! lazyElement.dataset.src ) {
+					if ( ! getData( lazyElement, 'src' ) ) {
 						lazyObserver.unobserve( lazyElement );
 						return;
 					}
-					lazyElement.src = lazyElement.dataset.src;
-					delete lazyElement.dataset.src;
-					if ( lazyElement.dataset.srcset ) {
-						lazyElement.srcset = lazyElement.dataset.srcset;
-						delete lazyElement.dataset.srcset;
+					lazyElement.src = getData( lazyElement, 'src' );
+					deleteData( lazyElement, 'src' );
+					if ( getData( lazyElement, 'srcset' ) ) {
+						lazyElement.srcset = getData( lazyElement, 'srcset' );
+						deleteData( lazyElement, 'srcset' );
 					}
-					if ( lazyElement.dataset.sizes ) {
-						lazyElement.sizes = lazyElement.dataset.sizes;
-						delete lazyElement.dataset.sizes;
+					if ( getData( lazyElement, 'sizes' ) ) {
+						lazyElement.sizes = getData( lazyElement, 'sizes' );
+						deleteData( lazyElement, 'sizes' );
 					}
 					lazyElement.classList.remove( 'native-lazyload-js-fallback' );
 					lazyObserver.unobserve( lazyElement );
@@ -59,16 +97,16 @@
 				setTimeout( function() {
 					lazyElements.forEach( function( lazyElement ) {
 						if ( ( lazyElement.getBoundingClientRect().top <= window.innerHeight && 0 <= lazyElement.getBoundingClientRect().bottom ) && 'none' !== getComputedStyle( lazyElement ).display ) {
-							if ( lazyElement.dataset.src ) {
-								lazyElement.src = lazyElement.dataset.src;
-								delete lazyElement.dataset.src;
-								if ( lazyElement.dataset.srcset ) {
-									lazyElement.srcset = lazyElement.dataset.srcset;
-									delete lazyElement.dataset.srcset;
+							if ( getData( lazyElement, 'src' ) ) {
+								lazyElement.src = getData( lazyElement, 'src' );
+								deleteData( lazyElement, 'src' );
+								if ( getData( lazyElement, 'srcset' ) ) {
+									lazyElement.srcset = getData( lazyElement, 'srcset' );
+									deleteData( lazyElement, 'srcset' );
 								}
-								if ( lazyElement.dataset.sizes ) {
-									lazyElement.sizes = lazyElement.dataset.sizes;
-									delete lazyElement.dataset.sizes;
+								if ( getData( lazyElement, 'sizes' ) ) {
+									lazyElement.sizes = getData( lazyElement, 'sizes' );
+									deleteData( lazyElement, 'sizes' );
 								}
 								lazyElement.classList.remove( 'native-lazyload-js-fallback' );
 							}
